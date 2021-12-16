f6c170a19d8d8ddbbb84c1a7a1e21e45f0c198cb
Gh0stage <61917966+Gh0stage@users.noreply.github.com>
2021-12-16 10:41:27 +0100

    nieuwe commit
    update van webdesign

---
+++ portfolio/resources/scripts/header.js
@@ -0,0 +1,11 @@
+var header = document.getElementsByTagName("header");
+    var headerHeight = header.height; 
+var logo = document.getElementById('logo');
+    var logoHeight = logo.height;
+scrollTo = screen.height;
+let yper = 0;
+
+$(window).on("scroll", function() {
+    alert("test");
+  });
+  
