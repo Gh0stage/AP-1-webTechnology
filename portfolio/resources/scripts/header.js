d2ad838553cbd8af1870aaf6b07becb9e9581e68
Gh0stage <61917966+Gh0stage@users.noreply.github.com>
2022-02-16 14:23:35 +0100

    update

--- portfolio/resources/scripts/header.js
+++ portfolio/resources/scripts/header.js
@@ -2,10 +2,11 @@ var header = document.getElementsByTagName("header");
     var headerHeight = header.height; 
 var logo = document.getElementById('logo');
     var logoHeight = logo.height;
-scrollTo = screen.height;
+scrollTo = screden.height;
 let yper = 0;
 
-$(window).on("scroll", function() {
+$window).on("scroll", function() {
     alert("test");
   });
+
   
